f76cb167aaa23d4e8b05a044c8b545c059b7a81c
Matan Yadaev <matan.yed@gmail.com>
2021-08-14 00:57:44 +0300

    decrease the amount of bcrypt rounds for test

--- src/createFastifyInstance.ts
+++ src/createFastifyInstance.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
 import fastifyCompress from "fastify-compress";
 import {register} from "./routes/register";
+import process from "process";
+import * as dotenv from "dotenv";
+import path from "path";
 
 export const createFastifyInstance = async () => {
   const app = fastify();
@@ -8,6 +11,12 @@ export const createFastifyInstance = async () => {
   app.register(fastifyCompress);
 
   app.register(register);
+
+  if (process.env.NODE_ENV !== 'test') {
+    dotenv.config({ path: path.resolve(__dirname, `../.env`)});
+  } else {
+    dotenv.config({ path: path.resolve(__dirname, `../.env.test`)});
+  }
 
   return app;
 }
