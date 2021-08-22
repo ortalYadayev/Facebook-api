02f991698e2d68eca06cbf007d9022e432368b97
ortal yadayev <ortalll20111@gmail.com>
2021-08-22 21:11:10 +0300

    fixes redis

--- src/redis.js
+++ src/redis.js
@@ -1,11 +1,13 @@
 import redis from "redis";
 
 const redisConf = {
-  auth_pass: process.env.REDIS_PASS,
+  password: process.env.REDIS_PASSWORD,
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT),
   no_ready_check: true,
 };
+
+console.log(redisConf);
 
 const client = redis.createClient(redisConf);
 
@@ -17,5 +19,3 @@ client.on("connect", () => {
   console.error("redis is connecting");
 });
 
-client.set("Channel", "CodeSpace", redis.print);
-client.get("Channel", redis.print);
