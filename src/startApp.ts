5ce6fc6bc29c4bf297e81da270ebe37f2b0d34ae
ortal yadayev <ortalll20111@gmail.com>
2021-08-28 22:26:03 +0300

    add port variable

--- src/startApp.ts
+++ src/startApp.ts
@@ -1,13 +1,15 @@
 import {createFastifyInstance} from "./createFastifyInstance";
 import {createConnection} from "typeorm";
 
+const PORT = 3000;
+
 export const startApp = async () => {
   const app = await createFastifyInstance();
 
   const connection = await createConnection();
 
   try {
-    await app.listen(3000)
+    await app.listen(PORT);
   } catch (error) {
     app.log.error(error);
     process.exit(1)
