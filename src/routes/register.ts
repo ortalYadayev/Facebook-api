50c2f5d46b667ebd793bafc3cdb769f452fe5060
ortal yadayev <ortalll20111@gmail.com>
2021-08-28 22:11:59 +0300

    remove bull

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -5,7 +5,6 @@ import bcrypt from 'bcrypt';
 import moment from "moment";
 import {URLToken} from "../entities/url_token.entity";
 import {sendMail} from "../services/mail.service";
-import Bull, {DoneCallback, Job} from "bull";
 
 const PayloadSchema = Type.Object({
   firstName: Type.String({minLength: 2, maxLength: 50}),
@@ -14,23 +13,6 @@ const PayloadSchema = Type.Object({
   password: Type.String({minLength: 8, maxLength: 255}),
 });
 type PayloadType = Static<typeof PayloadSchema>;
-
-const emailQueue = new Bull('email',{
-  redis: process.env.REDIS_URL
-});
-
-emailQueue.process((job: Job, done: DoneCallback) => {
-  setTimeout(() =>{
-    console.log("job processed");
-    console.log("job ID: ", job.id);
-    done();
-  },3000);
-});
-
-emailQueue.on('completed', (job: Job, result: any) => {
-  console.log('This message is from register');
-  console.log(job, result);
-});
 
 export const register = (app: FastifyInstance, options: FastifyPluginOptions, done: DoneFuncWithErrOrRes) => {
   app.post<{ Body: PayloadType }>('/register', {
@@ -69,14 +51,12 @@ export const register = (app: FastifyInstance, options: FastifyPluginOptions, do
     await urlToken.save()
 
     try {
-      await emailQueue.add(await sendMail({
-            to: user.email,
-            subject: "verify",
-            text: "email verification",
-            html: '<p>Click <a href="http://localhost:3000/verify?token=\'+${urlToken.token}+\'" >here</a> to verify your email</p>'
-          }), {
-        attempts: 2
-        });
+      await sendMail({
+        to: user.email,
+        subject: "verify",
+        text: "email verification",
+        html: '<p>Click <a href="http://localhost:3000/verify?token=\'+${urlToken.token}+\'" >here</a> to verify your email</p>'
+      });
     }
     catch(error) {
       console.log(error);
