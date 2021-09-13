7f1eb33d2521f005fb8886d536488b359338b3d0
ortal yadayev <ortalll20111@gmail.com>
2021-09-13 17:04:31 +0300

    wip

--- src/startApp.ts
+++ src/startApp.ts
@@ -1,16 +1,21 @@
 import { createConnection } from 'typeorm';
 import { FastifyInstance } from 'fastify';
+import fastifyCors from 'fastify-cors';
 import createFastifyInstance from './createFastifyInstance';
-
-const PORT = 8080;
 
 const startApp = async (): Promise<FastifyInstance> => {
   const app = await createFastifyInstance();
 
+  app.register(fastifyCors, {
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
+  });
+
   await createConnection();
 
   try {
-    await app.listen(PORT);
+    await app.listen(Number(process.env.APP_PORT));
   } catch (error) {
     app.log.error(error);
     process.exit(1);
