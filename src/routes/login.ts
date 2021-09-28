aff0603894e7f00635b3a25f346c598b789dc456
ortal yadayev <ortalll20111@gmail.com>
2021-09-28 14:55:54 +0300

    finish login page

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { Static, Type } from '@sinclair/typebox';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { User } from '../entities/user.entity';
 
 const PayloadSchema = Type.Object({
@@ -17,21 +18,26 @@ const login = (app: FastifyInstance): void => {
     handler: async (request, reply) => {
       const payload = request.body;
 
-      const existingUser = await User.findOne({
-        where: { email: payload.email },
+      const user: User | undefined = await User.findOne({
+        where: {
+          email: payload.email,
+        },
       });
 
-      if (
-        !existingUser ||
-        (existingUser &&
-          !(await bcrypt.compare(payload.password, existingUser.password)))
-      ) {
+      if (!user || !(await bcrypt.compare(payload.password, user.password))) {
         return reply.code(422).send({
           message: 'The email or password are incorrect',
         });
       }
 
-      return reply.code(201).send();
+      const secretKey = process.env.TOKEN_SECRET || '';
+
+      const token = jwt.sign(user.id.toString(), secretKey);
+
+      return reply.code(200).send({
+        token,
+        user,
+      });
     },
   });
 };
